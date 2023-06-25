eb3d104141d99a908022b26f69ce9ad8767137e3
KaremSouza12 <fulldevelop340@gmail.com>
2023-06-25 00:26:32 -0400

    change widget to statefull

--- lib/widgets/list_tile_week_values.dart
+++ lib/widgets/list_tile_week_values.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:my_notes/models/balance_week.dart';
+import 'package:my_notes/repositories/balance_week_repository.dart';
 
-class ListTileWeekValues extends StatelessWidget {
+class ListTileWeekValues extends StatefulWidget {
   const ListTileWeekValues({
-    super.key,
     required this.balance,
+    required this.repository,
+    required this.id,
+    super.key,
   });
 
   final BalanceWeek balance;
+  final BalanceWeekRepository repository;
+  final int? id;
+
+  @override
+  State<ListTileWeekValues> createState() => _ListTileWeekValuesState();
+}
+
+class _ListTileWeekValuesState extends State<ListTileWeekValues> {
+  Future<void> _showForm(
+    BuildContext context,
+    BalanceWeekRepository repository,
+  ) {
+    return showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        // return UpdateForm(nameDayWeek: null,);
+        return SizedBox();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +40,37 @@ class ListTileWeekValues extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: ListTile(
         title: Text(
-          balance.nameDayWeek as String,
+          widget.balance.nameDayWeek as String,
           style: const TextStyle(fontSize: 17),
         ),
         subtitle: Text(
-          balance.dateOfDayWeek as String,
+          widget.balance.dateOfDayWeek as String,
           style: const TextStyle(fontSize: 17),
         ),
-        trailing: SizedBox(
-          width: 100,
-          child: Text(
-            balance.valueOfDayWeek.toString(),
-            style: const TextStyle(fontSize: 17),
+        trailing: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          width: 120,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                widget.balance.valueOfDayWeek.toString(),
+                style: const TextStyle(fontSize: 17),
+              ),
+              IconButton(
+                onPressed: () => _showForm(context, widget.repository),
+                icon: const Icon(Icons.update),
+              ),
+              SizedBox(
+                width: 20,
+                child: IconButton(
+                  onPressed: () {
+                    widget.repository.deleteData(widget.id);
+                  },
+                  icon: const Icon(Icons.delete_forever),
+                ),
+              )
+            ],
           ),
         ),
       ),
